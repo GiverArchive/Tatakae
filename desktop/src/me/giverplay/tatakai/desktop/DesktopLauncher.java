920f823e3ba49702800a77f5981489c2cb77e653
GiverPlay007 <play.giver.007@gmail.com>
2021-01-07 14:21:38 -0300

    Versão do LWJGL alterada

--- desktop/src/me/giverplay/tatakai/desktop/DesktopLauncher.java
+++ desktop/src/me/giverplay/tatakai/desktop/DesktopLauncher.java
@@ -1,12 +1,16 @@
 package me.giverplay.tatakai.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import me.giverplay.tatakai.Tatakai;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Tatakai(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(1280, 720);
+		config.setResizable(false);
+		config.useVsync(true);
+
+		new Lwjgl3Application(new Tatakai(), config);
 	}
 }
