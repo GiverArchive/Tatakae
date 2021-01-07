7689f5247cce9888cd89ab49538bcf107bc07465
GiverPlay007 <play.giver.007@gmail.com>
2021-01-06 22:34:47 -0300

    Create LibGDX project

---
+++ desktop/src/me/giverplay/tatakai/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.giverplay.tatakai.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.giverplay.tatakai.Tatakai;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Tatakai(), config);
+	}
+}
---
+++ html/src/me/giverplay/tatakai/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package me.giverplay.tatakai.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import me.giverplay.tatakai.Tatakai;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Tatakai();
+        }
+}
