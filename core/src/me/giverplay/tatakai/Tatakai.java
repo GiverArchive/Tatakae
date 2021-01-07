66299f418655b109bf9b20eb0f8f66cdaa309a5a
GiverPlay007 <play.giver.007@gmail.com>
2021-01-07 15:40:17 -0300

    Setup do game.

--- core/src/me/giverplay/tatakai/Tatakai.java
+++ core/src/me/giverplay/tatakai/Tatakai.java
@@ -3,31 +3,61 @@ package me.giverplay.tatakai;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
-public class Tatakai extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+public final class Tatakai extends ApplicationAdapter
+{
+	public static final boolean DEBUG = false;
+
+	private static Tatakai instance;
+
+	private OrthographicCamera camera;
+	private Viewport viewport;
+	private SpriteBatch batch;
+
+	public Tatakai()
+	{
+		instance = this;
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void create ()
+	{
+		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera.setToOrtho(false);
+
+		viewport = new FillViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), camera);
+		batch = new SpriteBatch();
+	}
+
+	@Override
+	public void render ()
+	{
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+
 		batch.end();
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void resize(int width, int height)
+	{
+		viewport.update(width, height);
+	}
+
+	@Override
+	public void dispose ()
+	{
 		batch.dispose();
-		img.dispose();
+	}
+
+	public static Tatakai getInstance()
+	{
+		return instance;
 	}
 }
