974d3acd1a59a314b47aea404ee1f509829b93ab
GiverPlay007 <play.giver.007@gmail.com>
2021-01-11 21:31:40 -0300

    Criado um Asset Manager com a biblitoeca LibGDX Utils

---
+++ core/src/me/giverplay/tatakae/resource/Assets.java
@@ -0,0 +1,58 @@
+package me.giverplay.tatakae.resource;
+
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
+import com.badlogic.gdx.graphics.Texture;
+import net.dermetfan.gdx.assets.AnnotationAssetManager;
+import net.dermetfan.gdx.assets.AnnotationAssetManager.Asset;
+
+public class Assets
+{
+  public static final AnnotationAssetManager manager = new AnnotationAssetManager(new InternalFileHandleResolver());
+
+  @Asset
+  public static AssetDescriptor<Texture> stoneTexture = new AssetDescriptor<>("blocks/stone.png", Texture.class);
+
+  @Asset
+  public static AssetDescriptor<Texture> dirtTexture = new AssetDescriptor<>("blocks/stone.png", Texture.class);
+
+  @Asset
+  public static AssetDescriptor<Texture> cobblestoneTexture = new AssetDescriptor<>("blocks/stone.png", Texture.class);
+
+  @Asset
+  public static AssetDescriptor<Texture> obsidianTexture = new AssetDescriptor<>("blocks/stone.png", Texture.class);
+
+  @Asset
+  public static AssetDescriptor<Texture> bedrockTexture = new AssetDescriptor<>("blocks/stone.png", Texture.class);
+
+  public static void load()
+  {
+    Texture.setAssetManager(manager);
+    manager.load(Assets.class);
+  }
+
+  public static Texture getStoneTexture()
+  {
+    return manager.get(stoneTexture);
+  }
+
+  public static Texture getDirtStone()
+  {
+    return manager.get(dirtTexture);
+  }
+
+  public static Texture getCobblestoneTexture()
+  {
+    return manager.get(cobblestoneTexture);
+  }
+
+  public static Texture getObsidianTexture()
+  {
+    return manager.get(obsidianTexture);
+  }
+
+  public static Texture getBedrockTexture()
+  {
+    return manager.get(bedrockTexture);
+  }
+}
